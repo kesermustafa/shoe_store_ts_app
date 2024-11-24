46944e906c58280ead83076981548828ff52498c
Mustafa Keser <keserzade@gmail.com>
2024-11-24 14:04:21 +0300

    create packet

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/home";
+import Detail from "./pages/detail";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+	
+	return <BrowserRouter>
+		
+		<div className="min-h-screen bg-gray px-4 md:px-8 lg:px-12 xl:px-14 py-8">
+			<Header/>
+			<Routes>
+				<Route path="/" element={<Home/>}/>
+				<Route path="/detail/:id" element={<Detail/>}/>
+			</Routes>
+		</div>
+	
+	</BrowserRouter>
 }
 
 export default App
